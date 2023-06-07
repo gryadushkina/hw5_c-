98edd4c3c5f2d07f1bd4089a71eb1e657d20c37b
gryadushkina <griadushkina@gmail.com>
2023-06-08 00:29:23 +0300

    h/w

---
+++ задача34/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+Console.Clear();
+int[] array = GetRandomArray(12,100,999);
+int a = GetEvenCount(array);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"количество четных чисел в массиве равно {a}");
+
+int GetEvenCount(int[] array2) 
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el % 2 == 0) count += 1;
+    }
+    return count;
+}
+
+int[] GetRandomArray(int size,int minValue,int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue,maxValue+1);
+    }
+
+    return result;
+}
+
---
+++ задача38/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76*/
+
+Console.Clear();
+double[] array = GetRandomArray(12,100,999);
+double a = MaxMinNumber(array);
+Console.WriteLine(String.Join(", ", array));
+Console.WriteLine($"разница между максимальным и минимальным элементов массива равна  {a}");
+
+double[] GetRandomArray(int size,int minValue,int maxValue)
+{
+    double[] result = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue,maxValue+1);
+    }
+
+    return result;
+}
+
+double MaxMinNumber(double[] array2)
+{
+    double max = array2[0];
+    double min = array2[1];
+    for (int i = 0; i < array2.Length; i++)
+    {
+        if (array2[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array2[i] < min)
+        {    
+            min = array[i];
+        }
+    }
+    
+    double rez = max - min; 
+    return rez;
+    
+}
